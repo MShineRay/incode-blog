40453af5e173118e195c06ea5052e33e24d9a99d
anja-webcode <aleksandra.vucetic@gmail.com>
2021-04-14 08:14:59 +0200

    fetch posts from ghost

--- incode-blog/pages/index.tsx
+++ incode-blog/pages/index.tsx
@@ -1,7 +1,31 @@
 import Head from 'next/head'
 import styles from './../styles/Home.module.scss'
 
-export default function Home() {
+const { BLOG_URL, CONTENT_API_KEY } = process.env
+
+type Post = {
+  title: string
+  slug: string
+  custom_excerpt: string
+}
+
+async function getPosts() {
+  const res = await fetch(
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt`,
+  ).then(res => res.json())
+  return res.posts
+}
+
+export const getStaticProps = async ({ params }) => {
+  const posts = await getPosts()
+  return {
+    props: { posts },
+    revalidate: 10000,
+  }
+}
+
+const Home: React.FC<{ posts: Post[] }> = props => {
+  const { posts } = props
   return (
     <div className={styles.container}>
       <Head>
@@ -9,6 +33,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Blog</h1>
+      <ul>
+        {posts.map(post => {
+          return (
+            <li key={post.slug}>
+              <h3>{post.title}</h3>
+              <p>{post.custom_excerpt}</p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
+
+export default Home
