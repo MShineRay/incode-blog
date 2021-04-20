35b30ba7850fe0c0158a4d85aa52af73d5bb7d01
anja-webcode <aleksandra.vucetic@gmail.com>
2021-04-20 13:25:32 +0200

    next/link instead of router

--- incode-blog/pages/404.tsx
+++ incode-blog/pages/404.tsx
@@ -1,15 +1,18 @@
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 import Layout from '~/components/Layout/Layout'
 import styles from '~/styles/CustomError.module.scss'
 const Custom404 = () => {
-  const router = useRouter()
   return (
     <Layout pageTitle="Incode - 404">
       <div className={styles.incd_custom_404}>
         <div className={styles.incd_custom_404_content}>
           <h1>Oops, page not found</h1>
           <p>404</p>
-          <button onClick={() => router.push('/')}>Go back</button>
+          <Link href="/" passHref>
+            <a>
+              <button>Go back</button>
+            </a>
+          </Link>
           <img src={'/images/custom-404.svg'} alt="404 not found" />
         </div>
       </div>
