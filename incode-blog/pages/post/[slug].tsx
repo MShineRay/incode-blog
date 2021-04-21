5798305207bfd93993276c858db5ae7752950b06
anja-webcode <aleksandra.vucetic@gmail.com>
2021-04-21 11:33:50 +0200

    added logic for related posts

--- incode-blog/pages/post/[slug].tsx
+++ incode-blog/pages/post/[slug].tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import ErrorPage from 'next/error'
 import { useRouter } from 'next/router'
 import { GetStaticPaths, GetStaticProps } from 'next'
-import { getPosts, getSinglePost } from '~/api/api'
+import { getPosts, getSinglePost, getPostsByTag } from '~/api/api'
 import PostType from '~/types/post'
 import Layout from '~/components/Layout/Layout'
 import PostImage from '~/components/PostImage/PostImage'
@@ -11,8 +11,9 @@ import styles from '~/styles/Home.module.scss'
 
 type PostProps = {
   post: PostType
+  relatedPosts: PostType[]
 }
-const Post: React.FC<PostProps> = ({ post }: PostProps) => {
+const Post: React.FC<PostProps> = ({ post, relatedPosts }: PostProps) => {
   const router = useRouter()
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
@@ -48,6 +49,31 @@ const Post: React.FC<PostProps> = ({ post }: PostProps) => {
             <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
           </>
         )}
+        {relatedPosts.length !== 0 && (
+          <>
+            <h2>Related posts</h2>
+            <div className={styles.related_posts_container}>
+              <div className={styles.post_row}>
+                {relatedPosts.map(relatedPost => (
+                  <div key={relatedPost.slug} className={styles.post_item}>
+                    <Link
+                      href="../post/[slug]"
+                      as={`../post/${encodeURIComponent(relatedPost.slug)}`}
+                    >
+                      <a>
+                        <PostImage
+                          imageSrc={relatedPost.feature_image}
+                          title={relatedPost.title}
+                        />
+                        <p>{relatedPost.title}</p>
+                      </a>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   )
@@ -57,8 +83,11 @@ export default Post
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const post = await getSinglePost(params.slug as string)
+  const tagSlug = post?.primary_tag?.slug
+  const allRelatedPosts = (tagSlug && (await getPostsByTag(tagSlug))) || []
+  const relatedPosts = allRelatedPosts.filter(item => item.slug !== post.slug)
   return {
-    props: { post },
+    props: { post, relatedPosts },
     revalidate: 10000,
   }
 }
