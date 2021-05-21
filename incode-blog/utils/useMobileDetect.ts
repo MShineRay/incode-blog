e97f79b851156ad84ef1f694c2f040b6d834da01
anja-webcode <aleksandra.vucetic@gmail.com>
2021-05-21 16:06:18 +0200

    check if component is mounted before update state

--- incode-blog/utils/useMobileDetect.ts
+++ incode-blog/utils/useMobileDetect.ts
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react'
-// Utils
 import RegexEnum from './RegexEnum'
 
 export default function useMobileDetect() {
   const [isMobile, setMobile] = useState(false)
   useEffect(() => {
+    let isMounted = true
     const userAgent =
       typeof window.navigator === 'undefined' ? '' : navigator.userAgent
     const mobile = Boolean(userAgent.match(RegexEnum.MOBILE_PATTERN))
-    setMobile(mobile)
+    if (isMounted) {
+      setMobile(mobile)
+    }
+    return () => {
+      isMounted = false
+    }
   }, [])
   return { isMobile }
 }
