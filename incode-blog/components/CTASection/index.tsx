70b7cc042ffbdb34f12baa93fa18d27ec48490a2
anja-webcode <aleksandra.vucetic@gmail.com>
2021-07-22 11:16:56 +0200

    added mobile styles

--- incode-blog/components/CTASection/index.tsx
+++ incode-blog/components/CTASection/index.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router'
+import RoutesEnum from '~/utils/RoutesEnum'
 import styles from './CTASection.module.scss'
 
 const CTASection = () => {
   const router = useRouter()
   return (
     <div className={styles.cta}>
-      <button onClick={() => router.push('https://incode.com/contact/')}>
+      <button onClick={() => router.push(RoutesEnum.CONTACT)}>
         Let&apos;s get in touch
       </button>
     </div>
